e8de6655c8ffcd654e49dacede0a3b399b2c0d6f
obrunofontana <obrunofontana@gmail.com>
2022-06-08 19:31:26 -0300

    feat: 08/06 sobe ações iniciais para que continuem

--- example-list-message-from-array/main.js
+++ example-list-message-from-array/main.js
@@ -5,20 +5,36 @@ const loadPeoples = () => {
   return itemsJaArmazenados ? JSON.parse(itemsJaArmazenados) : [];
 }
 
-const span = () => {
+const onClickEdit = (element) => {
+  console.log('on click edit', element);
+}
+
+const onClickRemove = (element) => {
+  console.log('on click delete', element);
+}
+
+
+const span = (identificador) => {
   const span = document.createElement('span');
   const iconEdit = document.createElement('i');
   iconEdit.setAttribute('class', 'fas fa-edit');
   iconEdit.setAttribute('title', 'Editar');
+
+  iconEdit.setAttribute('identificador', `${identificador}`);
+  iconEdit.setAttribute('onclick', `onClickEdit(this)`);
   iconEdit.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
+
   span.appendChild(iconEdit);
 
   const iconRemove = document.createElement('i');
   iconRemove.setAttribute('class', 'fas fa-trash');
   iconRemove.setAttribute('title', 'Remover');
+  iconRemove.setAttribute('identificador', `${identificador}`);
+  iconRemove.setAttribute('onclick', `onClickRemove(this)`);
   iconRemove.setAttribute('style', 'cursor:pointer; margin-inline: 1rem;');
-  span.appendChild(iconRemove);
 
+  span.appendChild(iconRemove);
+  
   return span;
 } 
 
@@ -32,7 +48,7 @@ const listPeoples = () => {
   
   ul = document.createElement('ul');
  
-  peoples.forEach((item, index) => {
+  peoples.forEach((item, identificador) => {
     const li = document.createElement('li');
     li.innerHTML = 
     
@@ -40,8 +56,8 @@ const listPeoples = () => {
      Idade: ${item.age}, 
      Altura: ${item.height}
      `;
-    
-    li.appendChild(span())
+
+    li.appendChild(span(identificador))
     ul.appendChild(li);
   });
   
