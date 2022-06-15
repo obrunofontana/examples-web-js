7e57ab91f0fa93d93e10e5513e803ce8ca76b0e9
obrunofontana <obrunofontana@gmail.com>
2022-06-15 19:14:00 -0300

    feat: 15/06 sobe filter

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('keydown', function() {
+  const inputValue = document.querySelector('input').value;
+  const arrayItems = ['banana', 'goiaba', 'xpto', 'foo', 'bar'];
+
+  const carsFiltered = arrayItems.filter(item => item.includes(inputValue));
+  console.log('-> encontrou ',carsFiltered);
+})
